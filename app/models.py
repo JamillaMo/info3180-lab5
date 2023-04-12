faa188d1f4dd4cfff4d4a58d283c67404140ba6b
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-04-12 00:50:34 -0500

    Completed Exercise 2

--- app/models.py
+++ app/models.py
@@ -1,1 +1,15 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+import datetime
+from sqlalchemy import Column, Integer, DateTime
+from pytz import timezone
+
+tz = timezone('EST')
+#datetime.now(tz) 
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    description = db.Column(db.Text(255), unique=True)
+    poster = db.Column(db.String(80), unique=True)
+    created_at = Column(DateTime, default=datetime.datetime.now) #Check
