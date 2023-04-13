b1c9206b8696e8f3e1783ee6c39f97e126e9a807
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-04-12 21:04:02 -0500

    Did Part of Exercise 4

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,31 @@
+<!-- @submit.prevent="saveMovie" -->
+
+<script setup>
+    fetch("/api/v1/movies", {
+    method: 'POST'
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    // display a success message
+    console.log(data);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+</script>
+
+<!-- fetch("/api/v1/movies", {
+ method: 'POST'
+})
+ .then(function (response) {
+ return response.json();
+ })
+ .then(function (data) {
+ // display a success message
+ console.log(data);
+ })
+ .catch(function (error) {
+ console.log(error);
+ }); -->
